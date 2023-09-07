1f95646039b906fd6d86df51abcc9ff7fa03d00f
Grace Ejimkonye <74141427+gracious136@users.noreply.github.com>
2023-09-07 21:25:39 +0200

    Update the data links Home.py

--- Streamlit App/Home.py
+++ Streamlit App/Home.py
@@ -10,15 +10,15 @@ st.set_page_config(page_title='SMG Movies', page_icon ="🎬", layout="wide", in
 
 
 #readin my movie data
-movies = pd.read_csv('D:/Documents/DataScience2/WBS/MyNotebooks/DataEngineeringProject/Movie/movies.csv')
-ratings = pd.read_csv('D:/Documents/DataScience2/WBS/MyNotebooks/DataEngineeringProject/Movie/ratings.csv')
-tags = pd.read_csv('D:/Documents/DataScience2/WBS/MyNotebooks/DataEngineeringProject/Movie/tags.csv')
-links = pd.read_csv('D:/Documents/DataScience2/WBS/MyNotebooks/DataEngineeringProject/Movie/links.csv')
-df = pd.read_csv('D:/Documents/DataScience2/WBS/MyNotebooks/DataEngineeringProject/MovieGenre.csv', encoding="ISO-8859-1", usecols=["imdbId", "Title", "Genre", "Poster"])
+movies = pd.read_csv('https://github.com/gracious136/Movie-Recommendation-App/blob/main/data/movies.csv')
+ratings = pd.read_csv('https://github.com/gracious136/Movie-Recommendation-App/blob/main/data/ratings.csv')
+tags = pd.read_csv('https://github.com/gracious136/Movie-Recommendation-App/blob/main/data/tags.csv')
+links = pd.read_csv('https://github.com/gracious136/Movie-Recommendation-App/blob/main/data/links.csv')
+df = pd.read_csv('https://github.com/gracious136/Movie-Recommendation-App/blob/main/data/MovieGenre.csv', encoding="ISO-8859-1", usecols=["imdbId", "Title", "Genre", "Poster"])
 df['title']= df['Title']
 df.drop('Title', axis =1, inplace=True)
 
-movie_model =pickle.load(open('D:/Documents/DataScience2/WBS/MyNotebooks/DataEngineeringProject/Movie/movie_model.sav', 'rb'))
+movie_model =pickle.load(open('https://github.com/gracious136/Movie-Recommendation-App/blob/main/data/movie_model.sav', 'rb'))
 
 movie_ratings = movies.merge(ratings, how = 'inner', on = 'movieId')
 
