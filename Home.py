5680e02aeb0b998ccf882ebea0f01a45c2e71d63
Grace Ejimkonye <74141427+gracious136@users.noreply.github.com>
2023-09-07 22:39:20 +0200

    Update Home.py

--- Home.py
+++ Home.py
@@ -11,15 +11,15 @@ st.set_page_config(page_title='SMG Movies', page_icon ="🎬", layout="wide", in
 
 
 #readin my movie data
-movies = pd.read_csv('https://github.com/gracious136/Movie-Recommendation-App/blob/main/data/movies.csv')
-ratings = pd.read_csv('https://github.com/gracious136/Movie-Recommendation-App/blob/main/data/ratings.csv')
-tags = pd.read_csv('https://github.com/gracious136/Movie-Recommendation-App/blob/main/data/tags.csv')
-links = pd.read_csv('https://github.com/gracious136/Movie-Recommendation-App/blob/main/data/links.csv')
-df = pd.read_csv('https://github.com/gracious136/Movie-Recommendation-App/blob/main/data/MovieGenre.csv', encoding="ISO-8859-1", usecols=["imdbId", "Title", "Genre", "Poster"])
+movies = pd.read_csv('https://raw.githubusercontent.com/gracious136/Movie-Recommendation-App/main/data/movies.csv')
+ratings = pd.read_csv('https://raw.githubusercontent.com/gracious136/Movie-Recommendation-App/main/data/ratings.csv')
+tags = pd.read_csv('https://raw.githubusercontent.com/gracious136/Movie-Recommendation-App/main/data/tags.csv')
+links = pd.read_csv('https://raw.githubusercontent.com/gracious136/Movie-Recommendation-App/main/data/links.csv')
+df = pd.read_csv('https://raw.githubusercontent.com/gracious136/Movie-Recommendation-App/main/data/MovieGenre.csv', encoding="ISO-8859-1", usecols=["imdbId", "Title", "Genre", "Poster"])
 df['title']= df['Title']
 df.drop('Title', axis =1, inplace=True)
 
-movie_model =pickle.load(open('https://github.com/gracious136/Movie-Recommendation-App/blob/main/data/movie_model.sav', 'rb'))
+movie_model =pickle.load(open('https://github.com/gracious136/Movie-Recommendation-App/raw/main/data/movie_model.sav', 'rb'))
 
 movie_ratings = movies.merge(ratings, how = 'inner', on = 'movieId')
 
