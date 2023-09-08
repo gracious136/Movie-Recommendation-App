ad4f98bf1ece39709ffca38bb804059ed03d0ad2
Grace Ejimkonye <74141427+gracious136@users.noreply.github.com>
2023-09-08 13:28:41 +0200

    Update SMG Developers.py

--- pages/SMG Developers.py
+++ pages/SMG Developers.py
@@ -4,22 +4,22 @@ st.title('Meet the Creators of SGM Movies')
 
 
 
-col1, col2= st.columns(2)
+#col1, col2= st.columns(2)
 
-with col1:
-   st.header("Sebastian")
-   st.image("https://github.com/gracious136/Movie-Recommendation-App/blob/main/data/seba.png")#, output_format ='png')
-   st.write('''
+#with col1:
+st.header("Sebastian")
+st.image("https://github.com/gracious136/Movie-Recommendation-App/blob/main/data/seba.png")#, output_format ='png')
+st.write('''
    Sebastian is a Junior Data Analyst and a valuable member of the team who contributes greatly to work culture. He is positive and brings this to every project and meeting. His journey into the world of data science is a unique one, having embarked on this path later in life after a successful career running businesses. This background has given Sebastian an all-round business acumen, allowing him to possess an in-depth  understanding of how businesses operate from the ground up.
 His ability to bridge different departments and manage collaborations with contractors is a testament to his exceptional leadership and teamwork skills. He seamlessly integrates the data science domain with various aspects of business operations, ensuring that projects align with overarching business goals.
 Sebastian strength lies with his knack for visualising and presenting data. Transforming complex datasets into clear, insightful visual representations. Whether it's through graphs, charts, or interactive dashboards, He is comfortable conveying the story hidden within the data and most importantly making it accessible and actionable for all stakeholders. The art of communication and interpretation of data is paramount and Sebastian excels in this skill which takes second place to code and other technical skills.''') 
 
 
-with col2:
-   st.header("Mirella")
-   st.image("https://github.com/gracious136/Movie-Recommendation-App/blob/main/data/mire.png")#, format ='image/png')
-   st.write('''
-   Mirella is our junior Data Analyst who started her career as an Executive Personal Assistant. She worked in tandem with some of the best CEOs in Germany. It was in this world she harnessed her remarkable organisational & analytical skills. This grounding has made her excellent in spotting and uncovering valuable insights in data sets. Her attention to detail allowed her to see anomalies and discrepancies within the data that go unnoticed by others.
+#with col2:
+st.header("Mirella")
+st.image("https://github.com/gracious136/Movie-Recommendation-App/blob/main/data/mire.png")#, format ='image/png')
+st.write('''
+Mirella is our junior Data Analyst who started her career as an Executive Personal Assistant. She worked in tandem with some of the best CEOs in Germany. It was in this world she harnessed her remarkable organisational & analytical skills. This grounding has made her excellent in spotting and uncovering valuable insights in data sets. Her attention to detail allowed her to see anomalies and discrepancies within the data that go unnoticed by others.
 Mirella's critical thinking enables her to delve deeper into the data, constantly asking probing questions to understand the underlying trends and correlations. Using her research skills she effortlessly gathers additional domain context from the ever complex and comprehensive ‘internet of things’. In her role, Mirella's passion for seeking answers and her unwavering dedication to precision makes her an invaluable asset. She also brings and gentleness and kindness in the way she interacts with others, that creates a calming atmosphere even in the most stressful times.''')
 
 
