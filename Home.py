5046b5175ef6e5ae86329dde1f2d15cb6614dc2e
Grace Ejimkonye <74141427+gracious136@users.noreply.github.com>
2023-09-08 11:09:10 +0200

    Update Home.py to change number of raters and rating

--- Home.py
+++ Home.py
@@ -95,8 +95,8 @@ def related_movies(title):
     movie_df['common_movie_raters'] = no_of_rating_for_both_movies 
     results = movie_df[movie_df['common_movie_raters']>=5].reset_index()\
                 .merge(movie_ratings, how='inner', on ='movieId')\
-                .groupby(['movieId','title']).agg({'rating':'mean', 'common_movie_raters' : 'mean'}).reset_index()\
-                .sort_values('rating', ascending=False).head(25).merge(df, how = 'inner', on = 'title')
+                .groupby(['movieId','title']).agg({'rating':'mean', 'common_movie_raters' : 'sum'}).reset_index()\
+                .sort_values(by=['rating', 'common_movie_raters'], ascending=[False, False]).head(25).merge(df, how = 'inner', on = 'title')
 
     results['rating'] = round(results['rating'], 1)
     return results[['title','rating', 'Poster']].head(5)
